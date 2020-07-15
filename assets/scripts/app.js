07471c586921ebcaa5d12bab7bd5c8002f803ae1
tiagoveri <tiagoverisjc@gmail.com>
2020-07-14 23:47:05 -0300

    Added healing function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,14 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 //Valor a ser escolhido pelo user depois
 let chosenMaxLive = 100;
 let currentMonsterHealth = chosenMaxLive;
 let currentPlayerHealth = chosenMaxLive;
 
 adjustHealthBars(chosenMaxLive);
-
-function attackMonster(mode){
-    let maxDamage;
-    if(mode === 'ATTACK'){
-        maxDamage = ATTACK_VALUE;
-    } else if(mode === 'STRONG_ATTACK'){
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
-
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= damage;
+function endRound(){
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
@@ -29,6 +20,18 @@ function attackMonster(mode){
         alert('Empate');
     }
 }
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE;
+    } else if(mode === 'STRONG_ATTACK'){
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    endRound();
+}
 
 //Nome Handler pois faz interação com o EventListener
 function attackHandler(){
@@ -38,5 +41,20 @@ function attackHandler(){
 function strongAttackHandler(){
     attackMonster('STRONG_ATTACK');
 }
+function healPlayerHandler(){
+    let healValue;
+    if(currentPlayerHealth = chosenMaxLive - HEAL_VALUE){
+        alert('Você não pode se curar mais que sua vida inicial');
+        healValue = chosenMaxLive - currentPlayerHealth;
+    } else{
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
+
 attackBtn.addEventListener('click',attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click');
